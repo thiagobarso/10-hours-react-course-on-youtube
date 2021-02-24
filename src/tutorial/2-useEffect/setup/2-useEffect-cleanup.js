f290a0a3ff4195e7f87a6c2b42dc3aad36d7dc43
Thiago Barbosa Soares <thiagobarso@gmail.com>
2021-02-24 17:52:49 -0300

    5:30:35

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
-};
+  const [size, setSize] = useState(window.innerWidth)
 
-export default UseEffectCleanup;
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    console.log('useEffect')
+    window.addEventListener('resize', checkSize)
+    // return () => {
+    //   console.log('cleanup')
+    //   window.removeEventListener('resize', checkSize)
+    // }
+  }, [])
+
+  console.log('render')
+
+  return (
+    <>
+      <h2>Window</h2>
+      <h3>{size} PX</h3>
+    </>
+  )
+}
+
+export default UseEffectCleanup
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users'
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
-};
+  const [users, setUsers] = useState([])
 
-export default UseEffectFetchData;
+  const getUsers = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    setUsers(users)
+  }
+
+  return <h2>fetch data</h2>
+}
+
+export default UseEffectFetchData
